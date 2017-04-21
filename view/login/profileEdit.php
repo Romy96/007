b95e76b2abe98ac36a64df508bcd6964633768f0
Jurre Kon <99034766@mydavinci.nl>
2017-04-21 09:44:05 +0200

    Updated design of the edit form

--- view/login/profileEdit.php
+++ view/login/profileEdit.php
@@ -3,27 +3,31 @@
 	<div>
 		<input type="hidden" name="id" value="<?= $profile['id'];?>">
 		<label for="firstname">Firstname:</label>
-		<input type="text" name="firstname" value="<?= $profile['firstname'];?>">
+		<input class="form-control" type="text" name="firstname" value="<?= $profile['firstname'];?>">
 	</div>
 	<div>
 		<label for="prefix">Prefix:</label>
-		<input type="text" name="prefix" value="<?= $profile['prefix'];?>">
+		<input class="form-control" type="text" name="prefix" value="<?= $profile['prefix'];?>">
 	</div>
 	<div>
 		<label for="lastname">Lastname:</label>
-		<input type="text" name="lastname" value="<?= $profile['lastname'];?>">
+		<input class="form-control" type="text" name="lastname" value="<?= $profile['lastname'];?>">
 	</div>
 	<div>
 		<label for="username">Username:</label>
-		<input type="text" name="username" value="<?= $profile['username'];?>">
+		<input class="form-control" type="text" name="username" value="<?= $profile['username'];?>">
 	</div>
 	<div>
 		<label for="password">Password:</label>
-		<input type="password" name="password">
+		<div class="input-group margin-bottom-sm">
+		<span class="input-group-addon"><i class="fa fa-key fa-fw"></i></span>
+		<input class="form-control" type="password" name="password">
 	</div>
 	<div>
 		<label for="email">Email:</label>
-		<input type="text" name="email" value="<?= $profile['email'];?>">
+		<div class="input-group margin-bottom-sm">
+		<span class="input-group-addon"><i class="fa fa-envelope-o fa-fw"></i></span>
+		<input class="form-control" type="text" name="email" value="<?= $profile['email'];?>">
 	</div>
 		<label for="is_admin">Will this be an admin account?</label>
 		<label class="checkbox-inline"><input type="checkbox" id="yes" name="is_admin" value="1">Yes</label>
