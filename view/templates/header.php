12597258e51264f9e3d6aa9f4f8db9e5d406d905
Deona <deona.secreve@outlook.com>
2017-05-12 11:18:42 +0200

    bug fix

--- view/templates/header.php
+++ view/templates/header.php
@@ -17,7 +17,7 @@
 		<li><a href="<?= URL ?>login/logOut"><i class="fa fa-sign-out" aria-hidden="true"></i> Uitloggen </a></li>
 		<?php
 		if (isset($_SESSION['roles'])): 
-			if ($_SESSION['roles'] == "Admin"):
+			if ($_SESSION['roles'] = "Admin"):
 		?>
 		<li><a href="<?= URL ?>backend/index"><i class="fa fa-server" aria-hidden="true"></i> Beheer </a></li>
 		<?php
