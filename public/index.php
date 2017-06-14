ac8e5d5f3ba6a18f63b1f702c313cdcdf67ff8fb
Deona <deona.secreve@outlook.com>
2017-06-14 11:01:02 +0200

    Make comments in my own code

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ define("ROOT", dirname(__DIR__) . DIRECTORY_SEPARATOR);
 require(ROOT . "core/config.php");
 require(ROOT . "core/route.php");
 require(ROOT . "core/core.php");
-
+// let session work for 24 hours
 session_set_cookie_params(86400,"/");
 session_start();
 
