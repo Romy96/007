7ef5aabf78b1d406ab2f44a11f057d36d7cb8b82
Jurre Kon <99034766@mydavinci.nl>
2017-06-20 15:04:42 +0200

    created a button with which you can choose a file to upload. Also there is an upload function. There are still some bugs and errors in the function.

--- controller/BackendController.php
+++ controller/BackendController.php
@@ -376,7 +376,7 @@ function create_product()
 	}
 }
 
-function insert_product() 
+function insert_product()
 {
 	if ( IsLoggedInSession()==false ) {
 		echo "U heeft nog niet ingelogd";
@@ -402,6 +402,49 @@ function insert_product()
 
 		if(isset($_POST['product']) && isset($_POST['price']) && isset($_POST['category']) && isset($_POST['description']) && isset($_POST['amount']))
 		{
+			$target_dir = URL . "public/img/";
+			$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+			$uploadOk = 1;
+			$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+			// Check if image file is a actual image or fake image
+			if(isset($_POST["submit"])) {
+			    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+			    if($check !== false) {
+			        echo "File is an image - " . $check["mime"] . ".";
+			        $uploadOk = 1;
+			    } else {
+			        echo "File is not an image.";
+			        $uploadOk = 0;
+			    }
+			}
+			// Check if file already exists
+			if (file_exists($target_file)) {
+			    echo "Sorry, file already exists.";
+			    $uploadOk = 0;
+			}
+			// Check file size
+			if ($_FILES["fileToUpload"]["size"] > 500000) {
+			    echo "Sorry, your file is too large.";
+			    $uploadOk = 0;
+			}
+			// Allow certain file formats
+			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+			&& $imageFileType != "gif" ) {
+			    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+			    $uploadOk = 0;
+			}
+			// Check if $uploadOk is set to 0 by an error
+			if ($uploadOk == 0) {
+			    echo "Sorry, your file was not uploaded.";
+			// if everything is ok, try to upload file
+			} else {
+			    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+			        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+			    } else {
+			        echo "Sorry, there was an error uploading your file.";
+			    }
+			}
+
 			CreateProduct($_POST['product'], $_POST['price'], $_POST['category'], $_POST['description'], $_POST['amount']);
 			$products = AllProducts();
 			renderBackend("backend/products", array(
