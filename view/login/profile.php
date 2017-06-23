871077280ee241863eb37714777c2fb183e00c44
Deona <deona.secreve@outlook.com>
2017-06-23 11:37:06 +0200

    fix profile bug

--- view/login/profile.php
+++ view/login/profile.php
@@ -5,7 +5,7 @@
                 <div class="row">
                     <div class="col-sm-6 col-md-8">
                         <h4><?= $profile['firstname'];?> <?= $profile['prefix'] . " " . $profile['lastname'];?></h4>
-                        <small><cite title="San Francisco, USA"><?= $profile['home_adress'];?>, <?= $profile['zip_code'];?> <i class="glyphicon glyphicon-map-marker">
+                        <small><cite><?= $profile['home_adress'];?>, <?= $profile['zip_code'];?> <i class="glyphicon glyphicon-map-marker">
                         </i></cite></small>
                         <p>
                             <i class="glyphicon glyphicon-envelope"></i><?= $profile['email'];?>
